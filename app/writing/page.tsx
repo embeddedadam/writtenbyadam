9099ccd506b15b1c921f7d87b7b099132382bcf4
Adam Galecki <embeddedadam@gmail.com>
2024-06-22 20:37:31 +0200

    Update writing page layout with new styles and header
    Enhanced the writing page layout with improved structure and
    styling to boost readability and appearance. Added a centered
    container with a maximum width of 4XL and appropriate spacing.
    Included a header "Articles" with a horizontal rule below it
    to segment the content visually. Utilized flexbox for better
    alignment and distribution of elements within the page.

--- app/writing/page.tsx
+++ app/writing/page.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div className="container max-w-4xl py-6 lg:py-10 mx-auto">
+      <div className="flex justify-center flex-col items-center">
+        <div className="flex-1 space-y-4 text-center">
+          <h1>Articles</h1>
+          <hr className="border-gray-400 w-full" />
+        </div>
+      </div>
+    </div>
+  );
 }
